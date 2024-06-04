77415721d05e34d5ebec33d1f1c7171907182d31
masonwang025 <masonwang025@gmail.com>
2024-06-04 09:43:46 -0700

    link badge done

--- src/components/elements/LinkBadge.tsx
+++ src/components/elements/LinkBadge.tsx
@@ -1,5 +1,7 @@
 // inspo: https://leerob.io/
 
+import { DocumentIcon } from '@heroicons/react/24/outline'
+
 function LinkBadge({ link, text, newWindow, icon, className }: {
     link: string;
     text: string | React.ReactNode;
@@ -8,9 +10,12 @@ function LinkBadge({ link, text, newWindow, icon, className }: {
     className?: string;
 }) {
     return (
-        <a href={link} target={newWindow ? "_blank" : undefined} className={`trans inline-block bg-background-light border border-gray-400 px-2.5 py-[0.75] hover:bg-gray-50 rounded-lg ${className}`}>
-            {icon}
-            {text}
+        <a href={link} target={newWindow ? "_blank" : undefined} className={`trans inline-block bg-background-light border border-gray-400 px-2.5 py-[0.75] hover:bg-gray-50 rounded-lg ${className}`} rel={newWindow ? "noopener noreferrer" : undefined}>
+            <span className="horizontal center-v space-x-1.5">
+                {icon || <DocumentIcon className="h-4" />}
+                <span>{text}</span>
+                {newWindow && <span className="ml-1.5">↗</span>}
+            </span>
         </a>
     )
 }
