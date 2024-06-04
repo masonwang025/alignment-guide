6dceaaf65fd1b392166df10f5a147063d02fb0ef
masonwang025 <masonwang025@gmail.com>
2024-06-04 12:54:05 -0700

    added custom alert component

---
+++ src/components/elements/CustomAlert.tsx
@@ -0,0 +1,33 @@
+import { InformationCircleIcon } from "@heroicons/react/24/solid"
+import {
+    Alert,
+    AlertDescription,
+    AlertIcon,
+    AlertTitle,
+} from "@/components/ui/alert"
+
+interface AlertProps {
+    title: string | React.ReactNode;
+    description: string | React.ReactNode;
+    className?: string;
+    variant: string,
+    icon?: React.ElementType;
+}
+
+export function CustomAlert({ title, description, className, icon: Icon = InformationCircleIcon, variant }: AlertProps) {
+
+    return (
+        <Alert className={className} variant={variant}>
+            <AlertIcon icon={<Icon className="h-4 w-4" />} variant={variant} />
+            <div>
+                <AlertTitle className="font-semibold">{title}</AlertTitle>
+                <AlertDescription>
+                    {description}
+                </AlertDescription>
+            </div>
+        </Alert>
+    )
+}
+
+export default CustomAlert
+
