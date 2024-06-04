7b5d065766a68943effabdf825b33dde0d56c76c
tinamai <tinabmai@gmail.com>
2024-06-04 15:57:15 -0700

    before i kill snap scroll for the rest of the home page

--- src/components/home/Section.tsx
+++ src/components/home/Section.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 const Section = ({ id, children, className, fullWidth }: { id: string; children: React.ReactNode; className?: string; fullWidth?: boolean }) => {
-    const px = fullWidth ? 'px-0' : 'lg:px-24 md:px-16 sm:px-14 px-14';
+    const px = fullWidth ? 'px-0' : 'md:px-26 sm:px-20 px-14';
     return (
         <motion.section id={id} className={`snap-start flex flex-col vertical w-screen h-screen ${px} py-10 ${className}`}>
             {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
@@ -6,31 +6,16 @@ import { TypeAnimation } from 'react-type-animation';
 import Cursor from '../components/global/Cursor';
 import Section from '../components/home/Section';
 import separator from '../assets/separator.svg';
-import graphic from '../assets/magnificent-grants-image.png';
 import ChevronDown from '../assets/icons/chevron-down.svg';
+import { XMarkIcon } from '@heroicons/react/24/solid';
 
 export default function Index() {
     const [typingComplete, setTypingComplete] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = (event) => {
-            event.preventDefault();
-            const targetId = event.currentTarget.getAttribute('data-target');
-            const targetElement = document.getElementById(targetId);
-            targetElement?.scrollIntoView({ behavior: 'smooth' });
-        };
-
-        const scrollLinks = document.querySelectorAll('.scroll-link');
-        scrollLinks.forEach((link) => {
-            link.addEventListener('click', handleScroll);
-        });
-
-        return () => {
-            scrollLinks.forEach((link) => {
-                link.removeEventListener('click', handleScroll);
-            });
-        };
-    }, []);
+    const handleScroll = (targetId) => {
+        const targetElement = document.getElementById(targetId);
+        targetElement?.scrollIntoView({ behavior: 'smooth' });
+    };
 
     return (
         <div className='custom-cursor'>
@@ -41,7 +26,7 @@ export default function Index() {
                     <div />
                     <TypeAnimation sequence={['AI smarter than us will be our last invention.', 500, () => setTypingComplete(true)]} cursor={false} speed={60} repeat={0} className='font-serif sm:text-5xl text-4xl text-center' />
                     {typingComplete ? (
-                        <motion.div className='scroll-link flex flex-col vertical center-h' data-target='2' initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: 'easeInOut' }}>
+                        <motion.div className='flex flex-col vertical center-h' onClick={() => handleScroll('2')} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: 'easeInOut' }}>
                             <p className='text-lg pb-5 text-center'>So what?</p>
                             <motion.div animate={{ y: [0, -5, 0] }} transition={{ repeat: Infinity, repeatType: 'loop', duration: 1 }} style={{ zIndex: 5 }}>
                                 <Image src={ChevronDown} alt='chevron-down' width={30} height={30} />
@@ -56,14 +41,14 @@ export default function Index() {
                 <Section id='2' className='space-between vertical center-h'>
                     <div />
                     <div className='flex flex-col vertical center-h space-y-10'>
-                        <h1 className='font-serif lg:text-3xl text-2xl'>
+                        <h1 className='font-serif md:text-3xl text-2xl text-center'>
                             As AI nears and surpasses human-level intelligence, the fate of our species will depend on its actions. So we must figure out how to align it with humanity’s goals.
                         </h1>
                         <Image src={separator} alt='seperator' width={20} height={20} />
-                        <h1 className='font-serif lg:text-3xl text-2xl text-center'>Most people don’t know what AI alignment is, why it’s important, or what you can do to contribute.</h1>
+                        <h1 className='font-serif md:text-3xl text-2xl text-center'>Most people don’t know what AI alignment is, why it’s important, or what you can do to contribute.</h1>
                     </div>
 
-                    <div className='scroll-link flex flex-col vertical center-h' data-target='3'>
+                    <div className='flex flex-col vertical center-h' onClick={() => handleScroll('3')}>
                         <p className='text-lg pb-5 text-center'>Here's a guide to get started</p>
                         <motion.div animate={{ y: [0, -5, 0] }} transition={{ repeat: Infinity, repeatType: 'loop', duration: 1 }} style={{ zIndex: 5 }}>
                             <Image src={ChevronDown} alt='chevron-down' width={30} height={30} />
@@ -75,7 +60,7 @@ export default function Index() {
                 <Section id='3' className='space-between'>
                     <div />
                     <div>
-                        <h1 className='font-serif lg:text-5xl text-4xl'>What's this all about?</h1>
+                        <h1 className='font-serif md:text-5xl text-4xl'>What's this all about?</h1>
                         <div className='pt-12 space-y-7'>
                             <p>
                                 When you think of misaligned AI, you might think of the Terminator movies, but superintelligence doesn't need to be 'evil' to pose a threat. By default, all intelligent agents—including humans—aim
@@ -88,7 +73,7 @@ export default function Index() {
                             <p>We’re on the path to build something much harder to contain than nuclear weapons—superintelligent AI. Alignment is making sure we don’t accidentally end the world.</p>
                         </div>
                     </div>
-                    <div className='scroll-link flex flex-col vertical center-h' data-target='4'>
+                    <div className='flex flex-col vertical center-h' onClick={() => handleScroll('4')}>
                         <motion.div animate={{ y: [0, -5, 0] }} transition={{ repeat: Infinity, repeatType: 'loop', duration: 1 }} style={{ zIndex: 5 }}>
                             <Image src={ChevronDown} alt='chevron-down' width={30} height={30} />
                         </motion.div>
@@ -96,8 +81,63 @@ export default function Index() {
                 </Section>
 
                 {/* ——————— SECTION 4 ——————— */}
-                <Section id='4' className='center-v'>
-                    <h1 className='font-serif text-5xl pb-5 text-center'>Blah blah</h1>
+                <Section id='4' className='center-v space-y-5'>
+                    <h1 className='text-accent-600 uppercase tracking-[0.06rem] text-[0.85rem] pb-3'>To be clear, we are NOT:</h1>
+                    <div className='flex'>
+                        <span className='font-bold mr-5'>
+                            <XMarkIcon className='size-6' />
+                        </span>
+                        <div className='space-y-2'>
+                            <p>We do NOT advocate for slowing or pausing AI capabilities research.</p>
+                            <p>
+                                The best alignment methods have stemmed from advancements in AI capabilities—a trend we expect to continue. Moreover, pausing AI at this stage is completely unrealistic and undesirable due to
+                                overwhelming economic and technological incentives.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <span className='font-bold mr-5'>
+                            <XMarkIcon className='size-6' />
+                        </span>
+                        <div className='space-y-2'>
+                            <p>We are NOT suggesting that a misaligned AGI must possess consciousness or even seem as intelligent as a cat or dog.</p>
+                            <p>
+                                It doesn't need to harbor any animosity towards humans to pose a threat. Just as autonomous drones can be dangerous without consciousness, a misaligned AGI, with its significantly greater potential,
+                                could be far more hazardous.
+                            </p>
+                        </div>
+                    </div>
+                </Section>
+
+                {/* ——————— SECTION 5 ——————— */}
+                <Section id='5' className='center-v space-y-5'>
+                    <div className='space-y-5'>
+                        <h1 className='text-accent-600 uppercase tracking-[0.06rem] text-[0.85rem]'>So what are we doing here?</h1>
+                        <div className='flex'>
+                            <span className='font-bold mr-2'>1.</span>
+                            <p>We believe the development of AGI is the most important technology humans will ever create (and likely the last one too), and it is fundamental that it proceeds safely and correctly.</p>
+                        </div>
+                        <div className='flex'>
+                            <span className='font-bold mr-2'>2.</span>
+                            <div className='space-y-2'>
+                                <p>AGI Alignment, like the development of AGI itself, is a challenging technical problem.</p>
+                                <p>While fields such as philosophy and politics are vital for validating and ensuring the adoption of solutions, alignment remains a technical issue requiring a technical approach.</p>
+                            </div>
+                        </div>
+                        <div className='flex'>
+                            <span className='font-bold mr-2'>3.</span>
+                            <div className='space-y-2'>
+                                <p>Alignment is a solvable problem, but also an urgent one. </p>
+                                <p>
+                                    Leading AI companies predict AGI by 2029, and we need to solve alignment beforehand since we won't get another chance afterward. We have one critical try to get it right—unlike a mere
+                                    segmentation fault, misaligned AGI could have catastrophic consequences.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h1 className='font-serif text-5xl text-center'>Quickstart</h1>
+                    <p className='text-lg text-center pb-5'>What we think you should read or do first</p>
                     <div className='flex flex-col items-center justify-center space-y-5'>
                         <Link href='/quickstart'>
                             <p className='bg-background text-black py-2 px-4 rounded-lg shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)] transition-shadow duration-200 hover:shadow-[inset_0.2rem_0.2rem_0.5rem_rgb(225,226,228),inset_-0.2rem_-0.2rem_0.5rem_rgb(255,255,255)]'>
