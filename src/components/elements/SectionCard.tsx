c2c579010edf42ed8ac05db1b44398959fb1dfdb
masonwang025 <masonwang025@gmail.com>
2024-06-08 09:28:10 -0700

    quickstart cards done

--- src/components/elements/SectionCard.tsx
+++ src/components/elements/SectionCard.tsx
@@ -6,7 +6,7 @@ const SectionCard = ({ number, title, href, children }: { number: string; title:
         <Link href={href} className='flex flex-col px-7 lg:py-7 py-5 rounded-xl border border-gray-300 hover:bg-accent-300 hover:!border-transparent'>
             <div className='font-serif text-4xl italic pb-4'>{number}</div>
             <div className='font-serif font-semibold text-2xl'>{title}</div>
-            <div>{children}</div>
+            <div className="text-sm mt-2 text-gray-500">{children}</div>
         </Link>
     );
 };
