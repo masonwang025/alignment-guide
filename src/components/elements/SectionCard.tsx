6d962c0b073d6a7e5bc6352f15bbe4bc1a1e0210
tinamai <tinabmai@gmail.com>
2024-06-08 09:03:37 -0700

    quickstart card links are in too :)

--- src/components/elements/SectionCard.tsx
+++ src/components/elements/SectionCard.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Link from 'next/link';
 
-const SectionCard = ({ number, title, children }: { number: string; title: string; children: React.ReactNode }) => {
+const SectionCard = ({ number, title, href, children }: { number: string; title: string; href: string; children: React.ReactNode }) => {
     return (
-        <div className='flex flex-col px-7 lg:py-7 py-5 rounded-xl border border-gray-300 hover:bg-accent-300 hover:!border-transparent'>
+        <Link href={href} className='flex flex-col px-7 lg:py-7 py-5 rounded-xl border border-gray-300 hover:bg-accent-300 hover:!border-transparent'>
             <div className='font-serif text-4xl italic pb-4'>{number}</div>
             <div className='font-serif font-semibold text-2xl'>{title}</div>
             <div>{children}</div>
-        </div>
+        </Link>
     );
 };
 
