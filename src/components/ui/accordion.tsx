b353fa872395ca7e4e30da9bd4ef327dc02de989
masonwang025 <masonwang025@gmail.com>
2024-06-08 10:27:07 -0700

    checkpoint

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -27,7 +27,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center text-left justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 font-sans text-base text-slate-700 items-center text-left justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
