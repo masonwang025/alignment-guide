cb44a0af33a56ee6c1764fccd814ec615f9ef5c7
masonwang025 <masonwang025@gmail.com>
2024-06-08 10:19:45 -0700

    headers fixed

--- src/components/global/TextWrapper.tsx
+++ src/components/global/TextWrapper.tsx
@@ -6,12 +6,12 @@ interface TextHeaderWrapperProps {
 }
 
 const headingClasses = {
-    h1: 'font-serif text-5xl mt-5 mb-7 text-dark',
-    h2: 'font-serif text-3xl  mt-7 mb-7 pb-2 border-b border-gray-300 text-dark',
-    h3: 'font-serif text-2xl text-dark',
-    h4: 'font-serif text-xl text-dark',
-    h5: 'font-serif text-lg text-dark',
-    h6: 'font-serif text-base text-dark',
+    // h1: 'font-serif text-5xl mt-5 mb-7 text-dark',
+    // h2: 'font-serif text-3xl  mt-7 mb-7 pb-2 border-b border-gray-300 text-dark',
+    // h3: 'font-serif text-2xl text-dark',
+    // h4: 'font-serif text-xl text-dark',
+    // h5: 'font-serif text-lg text-dark',
+    // h6: 'font-serif text-base text-dark',
     p: 'font-sans text-base mt-3 nx-leading-7 first:nx-mt-0',
 };
 
