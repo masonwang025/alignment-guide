f5ced4cbbf160c1cfc062bf5d285810f83bae3b0
tinamai <tinabmai@gmail.com>
2024-06-08 16:57:06 -0700

    something shareable ;)))))

--- src/components/intro/Value.tsx
+++ src/components/intro/Value.tsx
@@ -1,12 +1,10 @@
-function Value({ iconComponent, children }: { iconComponent: React.ReactNode, children: React.ReactNode }) {
-    return (<div className='border border-gray-300 rounded-lg p-5 spacing-y-5 horizontal'>
-        <span className='font-bold mr-5'>
-            {iconComponent}
-        </span>
-        <div className='space-y-2'>
-            {children}
+function Value({ iconComponent, children }: { iconComponent: React.ReactNode; children: React.ReactNode }) {
+    return (
+        <div className='bg-accent-400 rounded-xl p-5 spacing-y-5 horizontal'>
+            <span className='font-bold mr-5'>{iconComponent}</span>
+            <div className='space-y-2'>{children}</div>
         </div>
-    </div>);
+    );
 }
 
 export default Value;
