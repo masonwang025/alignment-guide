8062dbb716699bb4797bcb07c2ffd6a873b1682a
masonwang025 <masonwang025@gmail.com>
2024-06-13 00:09:26 -0700

    fixed intro toggle (?)

--- src/components/intro/FalseCard.tsx
+++ src/components/intro/FalseCard.tsx
@@ -3,12 +3,12 @@ import Toggle from '../elements/Toggle';
 
 function FalseCard({ title, children }: { title: string; children: React.ReactNode }) {
     return (
-        <div className='flex'>
+        <div className='flex items-start'>
             <span className='font-bold mr-5'>
-                <XMarkIcon className='size-6 text-red-700' />
+                <XMarkIcon className='size-6 text-red-700 mt-2.5' />
             </span>
             <div className='space-y-2 w-full'>
-                <Toggle className='-mt-6' title={<p className='text-red-700 w-full'>{title}</p>}>
+                <Toggle className='' title={<p className='text-red-700 w-full'>{title}</p>}>
                     {children}
                 </Toggle>
             </div>
--- src/components/intro/Value.tsx
+++ src/components/intro/Value.tsx
@@ -2,12 +2,12 @@ import Toggle from "../elements/Toggle";
 
 function Value({ iconComponent, title, children }: { iconComponent: React.ReactNode; title: string | React.ReactNode; children?: React.ReactNode }) {
     return (
-        <div className='bg-accent-400 shadow-sm rounded-xl p-5 spacing-y-5 horizontal center-v'>
+        <div className='bg-accent-400 shadow-sm rounded-xl p-5 spacing-y-5 horizontal items-start'>
             <span className='mr-5'>{iconComponent}</span>
             <div className="flex-grow">
-                {children ? <Toggle className="-mt-3" title={<span className='mr-5'>{title}</span>}>
+                {children ? <div className="-mt-1"><Toggle className="" title={<span className='mr-5'>{title}</span>}>
                     <div className='space-y-2'>{children}</div>
-                </Toggle> : <div className="pr-2">{title}</div>}
+                </Toggle></div> : <div className="pr-2">{title}</div>}
             </div>
         </div>
     );
