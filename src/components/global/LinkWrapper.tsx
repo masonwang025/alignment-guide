2f266df7b81d752321ae172451fb5a2bf3ca1433
Tina <tinabmai@gmail.com>
2024-06-20 19:00:28 -0400

    alternate styling to links that looks *so good* but it's a little extra so it's there just in case lol

--- src/components/global/LinkWrapper.tsx
+++ src/components/global/LinkWrapper.tsx
@@ -8,6 +8,7 @@ interface LinkWrapperProps {
 const LinkWrapper: FC<LinkWrapperProps> = ({ children, href }) => {
     return (
         <a href={href} className='hover:text-accent-600 hover:text-opacity-70 underline underline-offset-4 decoration-1.5 decoration-dotted'>
+        {/* <a href={href} className='hover:text-opacity-70 hover:font-serif hover:italic hover:text-lg hover:leading-3 hover:tracking-wide underline underline-offset-4 decoration-1.5 decoration-dotted'> */}
             {children}
         </a>
     );
