c4a6db0fd62f2202e49a8cef2561eafccc975267
Tina <tinabmai@gmail.com>
2024-06-21 03:02:57 -0400

    contact card

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Contact() {
 	return (
-		<div className='my-6'>
+		<div className='mt-6 space-y-2'>
 			<div className='flex flex-col md:flex-row w-full space-x-0 md:space-x-2 space-y-2 md:space-y-0'>
 				<div className='md:w-1/2 w-full'>
 					<ContactCard name='Mason Wang' email='mwang25@stanford.edu' twitterLink='https://twitter.com/masonwang025' website='https://masonjwang.com'>
@@ -15,6 +15,26 @@ function Contact() {
 						<p>PhD student @ UPenn.</p>
 					</ContactCard>
 				</div>
+			</div>
+
+			<div className='px-7 lg:py-7 py-5 rounded-xl border border-neutral-300'>
+				<p className='inline'>Thank you to</p>
+				&nbsp;&nbsp;
+				<div className='inline-flex whitespace-nowrap text-neutral-500'>
+					<div className='flex items-center space-x-2 '>
+						<p className='text-[#334155] font-bold'>Tina Mai</p>
+						<a href={'https://twitter.com/t1namai'} target='_blank' rel='noopener noreferrer'>
+							<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' className='bi bi-twitter-x' viewBox='0 0 16 16'>
+								<path d='M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z' />
+							</svg>
+						</a>
+						<a href={'https://tinamai.xyz'} target='_blank' rel='noopener noreferrer'>
+							<GlobeAltIcon className='w-5 h-5' />
+						</a>
+					</div>
+				</div>
+				&nbsp;&nbsp;
+				<p className='inline'>for designing and helping with this website.</p>
 			</div>
 		</div>
 	);
